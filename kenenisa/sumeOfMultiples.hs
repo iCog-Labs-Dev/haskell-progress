043e8646adf29665a655548b924a435c2482ac23
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-02-20 12:30:56 +0300

    Create sumeOfMultiples.hs

---
+++ kenenisa/sumeOfMultiples.hs
@@ -0,0 +1,6 @@
+
+module SumOfMultiples (sumOfMultiples) where
+sumOfMultiples :: [Integer] -> Integer -> Integer
+sumOfMultiples factors limit = sum [num | num <- [1..limit],num < limit, isFactor num factors]
+                                where isFactor n xs = case xs of [] -> False
+                                                                 (x:xs) -> (x > 0 && (n `mod` x == 0)) || isFactor n xs
