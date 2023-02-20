6bcf9bb0b363a892afaa27d23d16e8e21921867b
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-02-20 12:25:56 +0300

    Create bob.hs

---
+++ kenenisa/bob.hs
@@ -0,0 +1,24 @@
+module Bob (responseFor) where
+import Data.Char (isSpace)
+lowerAlphabet :: [Char]
+lowerAlphabet = ['a' .. 'z']
+upperAlphabet = ['A' .. 'Z']
+containsUpperCase :: [Char] -> Bool
+containsUpperCase [] = False 
+containsUpperCase (x:xs) | x `elem` upperAlphabet = True | otherwise = containsUpperCase xs
+isAllCaps :: [Char] -> Bool
+isAllCaps [] = True
+isAllCaps (x : xs) | x `elem` lowerAlphabet = False | otherwise = isAllCaps xs
+isThereQuestionMark _ 0 = False
+isThereQuestionMark xs len | '?' == xs !! (len - 1) = True | isSpace (xs !! (len - 1)) = isThereQuestionMark xs (len - 1) | otherwise = False
+responseText question upperCased allCaps saidNothing 
+  | saidNothing = "Fine. Be that way!"
+  | upperCased && allCaps = if question then "Calm down, I know what I'm doing!" else "Whoa, chill out!"
+  | question = "Sure."
+  | otherwise = "Whatever." 
+responseFor :: String -> String
+responseFor xs = let question = isThereQuestionMark xs (length xs)
+                     upperCased = containsUpperCase xs
+                     allCaps = isAllCaps xs
+                     saidNothing = null xs || all isSpace xs
+                in responseText question upperCased allCaps saidNothing
