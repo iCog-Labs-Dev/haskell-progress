43c75d168b811166146fa8e67e09f68fe79d7238
meraf00 <meraf000@gmail.com>
2023-02-23 09:36:56 -0800

    Solution for https://exercism.org/tracks/haskell/exercises/collatz-conjecture

---
+++ leul/exercism/CollatzConjucture.hs
@@ -0,0 +1,12 @@
+module CollatzConjecture (collatz) where
+
+collatzList :: Integer -> [Integer]
+collatzList n
+  | n == 1 = []
+  | n `mod` 2 == 0 = n : collatzList ( floor $ (fromIntegral n) / 2)
+  | otherwise = n : collatzList (3 * n + 1)
+
+collatz :: Integer -> Maybe Integer
+collatz n
+    | n >= 1 = Just . fromIntegral . length $ collatzList n
+    | otherwise = Nothing
