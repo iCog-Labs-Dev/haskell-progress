321bb7c3db597d9bdf69a8d982f466725540a322
meraf00 <meraf000@gmail.com>
2023-02-28 17:31:53 -0800

    Files and command line args

--- leul/learnyouhaskel/9_io/files.hs
+++ leul/learnyouhaskel/9_io/files.hs
@@ -1,7 +1,21 @@
+import Control.Exception (handle)
+import Control.Monad (forever)
 import System.IO
-import Control.Monad ( forever )
+import Data.Char
 
-main = do         
-        fileHandle <- openFile "somefile.txt" ReadMode
-        content <- hGetContents fileHandle
+main = do
+  fileHandle <- openFile "somefile.txt" ReadMode
+  content <- hGetContents fileHandle
+  putStrLn content
+  hClose fileHandle
+
+  withFile
+    "somefile.txt"
+    ReadMode
+    ( \handle -> do
+        content <- hGetContents handle        
         putStrLn content
+    )
+
+  content <- readFile "somefile.txt"
+  writeFile "newfile.txt" (map toUpper content)
