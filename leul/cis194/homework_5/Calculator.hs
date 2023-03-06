a7dc496fd8aa0bf7c1546077e907d93e967a2e58
meraf00 <meraf000@gmail.com>
2023-03-06 09:49:45 -0800

    Add solution for HW5: https://www.seas.upenn.edu/~cis1940/spring13/hw/05-type-classes.pdf

--- leul/cis194/homework_5/Calculator.hs
+++ leul/cis194/homework_5/Calculator.hs
@@ -4,6 +4,63 @@ import ExprT ( ExprT(..) )
 import Parser ( parseExp )
 
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+
+class Expr a where
+    lit :: (Expr a) => Integer -> a
+    mul :: (Expr a) => a -> a -> a
+    add :: (Expr a) => a -> a -> a
+
+
+instance Expr Integer where
+    lit :: Integer -> Integer
+    lit = id
+    
+    add :: Integer -> Integer -> Integer
+    add a b = a + b
+    
+    mul :: Integer -> Integer -> Integer
+    mul a b = a * b
+
+
+instance Expr Bool where
+    lit :: Integer -> Bool
+    lit x
+        | x < 0 = False
+        | otherwise = True
+    
+    add :: Bool -> Bool -> Bool
+    add a b = a || b
+
+    mul :: Bool -> Bool -> Bool
+    mul a b = a && b
+
+
+instance Expr MinMax where
+    lit :: Integer -> MinMax
+    lit = MinMax
+
+    add :: MinMax -> MinMax -> MinMax
+    add (MinMax a) (MinMax b) = MinMax (max a b)
+
+    mul :: MinMax -> MinMax -> MinMax
+    mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+
+instance Expr Mod7 where
+    lit :: Integer -> Mod7
+    lit a = Mod7 (a `mod` 7)
+    
+    add :: Mod7 -> Mod7 -> Mod7
+    add (Mod7 a) (Mod7 b) = Mod7 (a + b `mod` 7)
+    
+    
+    mul :: Mod7 -> Mod7 -> Mod7
+    mul (Mod7 a) (Mod7 b) = Mod7 (a * b `mod` 7)
+
+
 
 eval :: ExprT -> Integer
 eval (Lit a) = a
