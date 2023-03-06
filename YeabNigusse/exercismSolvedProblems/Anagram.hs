2163cab33d7cdd7c8b89fee4520f3297ed08f9e7
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-03-06 11:29:56 +0300

    adding input output file

--- YeabNigusse/exercismSolvedProblems/Anagram.hs
+++ YeabNigusse/exercismSolvedProblems/Anagram.hs
@@ -1,6 +1,7 @@
 module Anagram (anagramsFor) where
 import Data.Char(toLower)
 import Data.List (sort)
+
 anagramsFor :: String -> [String] -> [String]
 anagramsFor xs xss = [ y | y <- xss, sortString xs == sortString y,map toLower xs /= map toLower y]
 
