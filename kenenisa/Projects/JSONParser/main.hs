d73b4dac61de1961d3d446f7f7240ad0ed51c667
kenenisa <kenenisaalemayhu0@gmail.com>
2023-03-06 14:52:22 +0300

    fixed data structure

--- kenenisa/Projects/JSONParser/main.hs
+++ kenenisa/Projects/JSONParser/main.hs
@@ -1,21 +1,12 @@
-data Value = String | Int | Bool | Object | Array | Null
+type NumberType = Int
+data Value = ValueString String | Number Int | Boolean Bool | Object [(String,Value)] | Array [Value] | Null 
+    deriving (Read,Show)
 
-type Key = String
 
-data ObjectData = ObjectData Key Value
+sample = "{\"profile\":{\"id\":1234,\"name\" : \"kenenisa alemayehu\" }, \"fiends\" :\n[{\"id\":3456,\"name\":\"John the official 123\"},{\"id\":9846,\"name\":\"ed true\"}]}"
 
-type Object = [ObjectData]
-
-type Number = Int
-
-type Boolean = Bool
-
-type Array = [Value]
-
-type JSON = Object
-
--- sample = "{\"profile\":{\"id\":1234,\"name\" : \"kenenisa alemayehu\" }, \"fiends\" :\n[{\"id\":3456,\"name\":\"John the official 123\"},{\"id\":9846,\"name\":\"ed true\"}]}"
-sample = "{content {is} this}wtf broo"
+digits :: String
+digits = ['0' .. '9']
 
 normalize :: [Char] -> Bool -> [Char]
 normalize [] _ = []
@@ -26,19 +17,50 @@ normalize (x : xs) outSide
   | x == '\n' || x == '\t' = normalize (tail xs) outSide
   | otherwise = x : normalize xs outSide
 
--- parseKeyValue :: String -> Object
--- parseKeyValue _ = []
--- parseKeyValue (x:xs) = 
+parseQuotes :: String -> Bool -> String -> (Value, String)
+parseQuotes [] _ _ = error "ERROR: unable to parse string"
+parseQuotes (x : xs) open acc
+  | x == '"' && open = (ValueString $ reverse acc, xs)
+  | x == '"' && not open = parseQuotes xs True acc
+  | otherwise = parseQuotes xs open (x : acc)
 
+parseNumber :: String -> String -> (Value, String)
+parseNumber (x : xs) acc
+  | x == ',' = (Number (read (reverse acc) :: Int), xs)
+  | x `notElem` digits = error "ERROR: unable to parse number"
+  | otherwise = parseNumber xs (x : acc)
 
-parseObject :: String -> String -> String -> String
+extractKey :: Value -> String
+extractKey (ValueString key) = key
+
+parseKeyValue :: String -> [(String,Value)]
+parseKeyValue [] = []
+parseKeyValue (x : xs) 
+  | x == '"' =  (extractKey key, fst value) : parseKeyValue (snd value)
+  | otherwise = parseKeyValue xs
+  where
+    parsedKey = parseQuotes xs False []
+    key = fst parsedKey
+    parsedValue = tail $ tail $ snd parsedKey
+    value :: (Value, String)
+    value
+      | head parsedValue == '{' =  (Object $ fst objectRule,snd objectRule)
+      | head parsedValue == '"' = parseQuotes parsedValue False []
+      | head parsedValue == 't' = (Boolean True, drop 4 parsedValue)
+      | head parsedValue == 'f' = (Boolean False, drop 5 parsedValue)
+      | head parsedValue == 'n' = (Null, drop 4 parsedValue)
+      | head parsedValue `elem` digits = parseNumber parsedValue []
+      | head parsedValue == '[' = error "Not implemented yet"
+      | otherwise = error "Error: don't know what to parse value to"
+        where objectRule = parseObject parsedValue [] []
+
+parseObject :: String -> String -> String -> ([(String,Value)],String)
 parseObject [] stack _ = error "ERROR: unable to parse object"
 parseObject (x : xs) stack content
-  | x == '}' && length stack == 1 = tail $ reverse content
+  | x == '}' && length stack == 1 =  (parseKeyValue $ tail $ reverse content, xs)
   | x == '}' = parseObject xs (tail stack) (x : content)
-  | x == '{' = parseObject xs (x:stack) (x:content)
-  | otherwise = parseObject xs stack (x:content)
-
+  | x == '{' = parseObject xs (x : stack) (x : content)
+  | otherwise = parseObject xs stack (x : content)
 
 fromJSON :: a
 fromJSON = error "hey"
@@ -52,4 +74,4 @@ printer (x : xs) = do
 main :: IO ()
 main = do
   printer sample
-  putStrLn $ parseObject sample [] []
+  print $ Object $ fst $ parseObject sample [] []
