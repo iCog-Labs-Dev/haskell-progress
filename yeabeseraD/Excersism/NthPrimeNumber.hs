a4ad7fbcd700cbbb6b36df57fdbe90bb00d9f013
Yagth <yabuman46@gmail.com>
2023-03-07 12:58:34 +0300

    Nth prime solved with the most efficient algorithm I can find

--- yeabeseraD/Excersism/NthPrimeNumber.hs
+++ yeabeseraD/Excersism/NthPrimeNumber.hs
@@ -4,12 +4,8 @@ nth :: Int -> Maybe Integer
 nth 0 = Nothing
 nth n = Just (primeNumbers !! (n-1))
 
-primeNumbers = [a | a <- [2..], isPrime a]
+primeNumbers = [a | a <- [2..], not $ isPrime a]
 
-isPrime :: Integer ->Bool
-isPrime a
-    | a < 4 = True
-    | even a = False
-    | otherwise  = null $ factors a
-    where factors a = filter (isFactor a) [b | b <- [2..(a-1)], odd b]
-          isFactor a b = a `mod` b == 0
+isPrime x = not $ all (\p -> x `mod` p /= 0) possibleFactors
+    where possibleFactors = takeWhile (\p -> p*p <= x) [2..(x-1)]
+        
