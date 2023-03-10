ba880e9fc4d0628460f569d2637026fc5812403a
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-03-10 03:12:16 +0300

    modifing file

--- YeabNigusse/LearnYouHaskellNotes/FunctorsAndMonoids.hs
+++ YeabNigusse/LearnYouHaskellNotes/FunctorsAndMonoids.hs
@@ -4,10 +4,23 @@ main = do line <- getLine
           putStrLn $ "You said " ++ line' ++ " backwards!"  
           putStrLn $ "Yes, you really said " ++ line' ++ " backwards!"
 --}
+main :: IO ()
 main = do 
-        putStrLn "what is your name please?"
+        putStrLn "what is your name please?" --functor with IO
         line <- fmap ("Hello "++) getLine
         putStrLn line
 -- instance Functor ((->) r) where
---     fmap f g =(\x -> f (g x))
+-- fmap f g =(\x -> f (g x))
+
+-- instance Functor Maybe where             instance of Maybe for functor typeclass
+--         fmap f (Just x) = Just (f x)  
+--         fmap f Nothing = Nothing  
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)  
+
+instance Functor CMaybe where  
+    fmap f CNothing = CNothing  
+    fmap f (CJust counter x) = CJust (counter+1) (f x) 
+-- Applicative functors
+
 
