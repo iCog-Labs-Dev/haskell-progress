61aa38282c3d95e366ea838dcadd03f9ba64fc34
Yagth <yabuman46@gmail.com>
2023-03-10 11:55:13 +0300

    Fixed look of skips:

--- yeabeseraD/Homework/Golf.hs
+++ yeabeseraD/Homework/Golf.hs
@@ -6,5 +6,7 @@ skips xs = init $ foldr (\x acc -> getEveryN xs x 0 : acc) [[]] [1..length xs]
     where getEveryN :: [a] -> Int -> Int -> [a]
           getEveryN xs 1 _ = xs
           getEveryN xs n index
-            | index < length xs = if (index+1) `mod` n == 0 then xs !! index : getEveryN xs n (index + 1) else getEveryN xs n (index + 1)
+            | index < length xs = if (index+1) `mod` n == 0 
+                                    then xs !! index : getEveryN xs n (index + 1) 
+                                    else getEveryN xs n $ index + 1
             | otherwise = []
