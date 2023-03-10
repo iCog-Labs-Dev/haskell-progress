476cd171916d529ebd17f0ebe55cec1d5e20ed1a
Yagth <yabuman46@gmail.com>
2023-03-10 15:47:50 +0300

    Finished the historgram

--- yeabeseraD/Homework/Golf.hs
+++ yeabeseraD/Homework/Golf.hs
@@ -1,5 +1,5 @@
 module Golf where 
-import Data.List (findIndex, elemIndex)
+import Data.List (findIndex, elemIndex, group, sort, transpose)
 
 skips :: [a] -> [[a]]
 skips xs = init $ foldr (\x acc -> getEveryN xs x 0 : acc) [[]] [1..length xs]
@@ -21,3 +21,10 @@ localMaxima xs = foldr (\x acc -> if isMaximum x xs then xs !! x : acc else acc)
             | otherwise = nums !! (index - 1) < n && nums !! (index + 1) < n
             
             where n = nums !! index
+
+histogram :: [Int] -> String
+histogram xs = unlines (histRows ++ [histBottom])
+    where counts = map (\n -> length $ filter (==n) xs) [0..9]
+          maxCount = maximum counts
+          histRows = reverse $ transpose $ map (\c -> replicate c '*' ++ replicate (maxCount - c) ' ') counts
+          histBottom = replicate 10 '=' ++ "\n" ++ concatMap show [0..9]
