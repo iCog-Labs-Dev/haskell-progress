b012244eb6ef81e728c237ae9f2730d5509f7033
Yagth <yabuman46@gmail.com>
2023-03-10 11:50:55 +0300

    Format established

--- yeabeseraD/Homework/Golf.hs
+++ yeabeseraD/Homework/Golf.hs
@@ -3,9 +3,8 @@ import Data.List (findIndex, elemIndex)
 
 skips :: [a] -> [[a]]
 skips xs = init $ foldr (\x acc -> getEveryN xs x 0 : acc) [[]] [1..length xs]
-
-getEveryN :: [a] -> Int -> Int -> [a]
-getEveryN xs 1 _ = xs
-getEveryN xs n index
-    | index < length xs = if (index+1) `mod` n == 0 then xs !! index : getEveryN xs n (index + 1) else getEveryN xs n (index + 1)
-    | otherwise = []
+    where getEveryN :: [a] -> Int -> Int -> [a]
+          getEveryN xs 1 _ = xs
+          getEveryN xs n index
+            | index < length xs = if (index+1) `mod` n == 0 then xs !! index : getEveryN xs n (index + 1) else getEveryN xs n (index + 1)
+            | otherwise = []
