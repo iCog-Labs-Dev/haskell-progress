310b450b30db5060cb6fba8eb9463019121ec4f2
Yagth <yabuman46@gmail.com>
2023-03-12 17:54:39 +0300

    Some wholemeal questions

---
+++ yeabeseraD/Excersism/BST.hs
@@ -0,0 +1,44 @@
+module BST
+    ( BST
+    , bstLeft
+    , bstRight
+    , bstValue
+    , empty
+    , fromList
+    , insert
+    , singleton
+    , toList
+    ) where
+
+data BST a = Nil | Node (BST a) a (BST a) deriving (Eq, Show)
+
+bstLeft :: BST a -> Maybe (BST a)
+bstLeft (Node left _ _) = Just left
+bstLeft Nil = Nothing
+
+bstRight :: BST a -> Maybe (BST a)
+bstRight (Node _ _ right) = Just right
+bstRight Nil = Nothing
+
+bstValue :: BST a -> Maybe a
+bstValue (Node _ curr _) = Just curr 
+bstValue Nil = Nothing
+
+empty :: BST a
+empty = Nil
+
+fromList :: Ord a => [a] -> BST a
+fromList = foldl (flip insert) empty
+
+insert :: Ord a => a -> BST a -> BST a
+insert x Nil = singleton x
+insert x (Node left a right)
+    | x <= a =Node (insert x left) a right
+    | otherwise =Node left a (insert x right)
+
+singleton :: a -> BST a
+singleton x = Node Nil x Nil
+
+toList :: BST a -> [a]
+toList Nil = []
+toList (Node left x right) = toList left ++ [x] ++ toList right
