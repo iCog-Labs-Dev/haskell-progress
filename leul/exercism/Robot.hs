5d81ae92784d62322e4b1914cf2b77ef14ddeb90
meraf00 <meraf000@gmail.com>
2023-03-13 13:20:45 +0300

    Add solution for https://exercism.org/tracks/haskell/exercises/robot-simulator

--- leul/exercism/Robot.hs
+++ leul/exercism/Robot.hs
@@ -1,6 +1,4 @@
 -- https://exercism.org/tracks/haskell/exercises/robot-simulator/
-
-
 module Robot
     ( Bearing(East,North,South,West)
     , bearing
@@ -8,6 +6,8 @@ module Robot
     , mkRobot
     , move
     ) where
+
+import Data.List ( elemIndex )
 
 data Bearing = North
              | East
@@ -20,12 +20,43 @@ data Robot =  Robot {
     coordinates :: (Integer, Integer)
 }
 
+fromJust :: Maybe a -> a
+fromJust (Just a) = a
+
 
 mkRobot :: Bearing -> (Integer, Integer) -> Robot
 mkRobot direction coordinates = Robot {bearing=direction, coordinates=coordinates}
 
+
+advance :: Bearing -> (Integer, Integer) -> (Integer, Integer)
+advance bearing coord = case bearing of
+                            North -> (fst coord, snd coord + 1)
+                            South -> (fst coord, snd coord - 1)
+                            East -> (fst coord + 1, snd coord)
+                            West -> (fst coord - 1, snd coord)
+
+
+turn :: Bearing -> Char -> Bearing
+turn bearing t = case t of 
+                    'R' -> bearings !! ((bearingIndex + 1) `mod` 4)
+                    'L' -> bearings !! ((bearingIndex - 1) `mod` 4)
+                
+                where bearings = [North, East, South, West]
+                      bearingIndex = fromJust $ bearing `elemIndex` bearings
+    
+
+
 move :: Robot -> String -> Robot
 move robot [] = robot
--- move robot (x:xs) = case x of
+move robot (x:xs) = move (step x) xs
+                    where 
+                          currentBearing = bearing robot
+                          currentCoord = coordinates robot    
+                          step x = case x of
+                                    'R' -> Robot {bearing = turn currentBearing 'R', coordinates = currentCoord}
+                                    'L' -> Robot {bearing = turn currentBearing 'L', coordinates = currentCoord}
+                                    'A' -> Robot {bearing = currentBearing, coordinates = advance currentBearing currentCoord}                                            
+                          
+
     
     
