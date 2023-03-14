f9b26f92e17097e2aa5aecc333b954fb8d5b0a06
Yagth <yabuman46@gmail.com>
2023-03-14 12:33:08 +0300

    Finished second Iteration of robot

--- yeabeseraD/Excersism/Robot.hs
+++ yeabeseraD/Excersism/Robot.hs
@@ -12,7 +12,7 @@ data Bearing = North
              | East
              | South
              | West
-             deriving (Eq, Show)
+             deriving (Eq, Show,Enum)
 
 data Robot = Robot {getDirection :: Bearing, getPosition :: (Integer, Integer)} deriving (Show)
 
@@ -27,24 +27,32 @@ mkRobot = Robot
 
 move :: Robot -> String -> Robot
 move robot "" = robot
-move robot@(Robot direction (x,y)) instructions = foldl (\newRobot x -> if x `elem` ["R", "L"] then changeDirection x newRobot else nextMove newRobot) robot instructionList
-    where nextMove (Robot newDir (x,y))
-            | newDir == North =Robot newDir (x,y+1)
-            | newDir == East = Robot newDir (x+1, y)
-            | newDir == South =Robot newDir  (x, y-1)
-            | otherwise = Robot newDir (x-1, y)
-          
-          changeDirection "R" (Robot newDir (x',y'))
-            | newDir == North = Robot East (x',y')
-            | newDir == East = Robot South (x',y')
-            | newDir == South = Robot West (x',y')
-            | otherwise = Robot North (x',y')
-          changeDirection "L" (Robot newDir (x',y'))
-            | newDir == North = Robot West (x',y')
-            | newDir == East = Robot North (x',y')
-            | newDir == South = Robot East (x',y')
-            | otherwise = Robot South (x',y')
-          instructionList = moves instructions
+move robot@(Robot direction (x,y)) instructions = foldl (flip chooseFunction) robot instructionList
+    where instructionList = moves instructions
+          chooseFunction "A" = advanceRobot
+          chooseFunction xs = changeDirection xs
+
+advanceRobot :: Robot -> Robot
+advanceRobot (Robot newDir (x,y)) = 
+    case newDir of
+    North -> Robot newDir (x,y+1)
+    East -> Robot newDir (x+1, y)
+    South -> Robot newDir  (x, y-1)
+    West -> Robot newDir (x-1, y)
+
+changeDirection:: String -> Robot -> Robot
+changeDirection "R" (Robot newDir position) = Robot (rotateRight newDir) position
+changeDirection "L" (Robot newDir position) = Robot (rotateLeft newDir) position
+
+
+rotateRight :: Bearing -> Bearing
+rotateRight West = North
+rotateRight dir = succ dir
+
+rotateLeft :: Bearing -> Bearing
+rotateLeft North = West
+rotateLeft dir = pred dir
+
 moves :: String -> [String]
 moves xs =words . intersperse ' ' $ cleanString
     where cleanString = filter (`elem` "RLA") ( map toUpper xs)
