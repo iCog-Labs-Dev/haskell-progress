7249ed735503291ab0898278c8811cb10e30cc10
Yagth <yabuman46@gmail.com>
2023-03-15 20:30:51 +0300

    Finished solving Homework 6

--- yeabeseraD/Homeworks/Homework 6/Streams.hs
+++ yeabeseraD/Homeworks/Homework 6/Streams.hs
@@ -1,7 +1,7 @@
 data Stream a = Stream {value :: a, cons :: Stream a}
 
 instance Show a => Show (Stream a) where
-    show stream = take 50 (show $ streamToList stream) ++ "...]"
+    show stream = show (take 20 (streamToList stream)) ++ "..."
 
 streamToList :: Stream a -> [a]
 streamToList (Stream value tailList) = value:streamToList tailList
@@ -9,14 +9,19 @@ streamToList (Stream value tailList) = value:streamToList tailList
 streamRepeat :: a -> Stream a
 streamRepeat value = Stream value (streamRepeat value)
 
-streamMap :: (a -> b) -> Stream a -> Stream b 
+streamMap :: (a -> b) -> Stream a -> Stream b
 streamMap f (Stream v tailStream) = Stream (f v) (streamMap f tailStream)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f seed = Stream seed (streamFromSeed f (f seed) )
 
 nats :: Stream Integer
-nats = getNaturalStream 0
+nats = streamFromSeed (+1) 0
 
-getNaturalStream :: Integer -> Stream Integer
-getNaturalStream n = Stream n (getNaturalStream $ n+1)
+ruler :: Stream Integer
+ruler = streamMap (`dividedByTwo` 0) nats
+
+dividedByTwo :: Integer -> Integer -> Integer
+dividedByTwo x count
+    | even x = dividedByTwo (x`div`2) count+1
+    | otherwise = count
