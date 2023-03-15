9e672f4d01638e1a9ba8dc539da1327476444fc8
Yagth <yabuman46@gmail.com>
2023-03-15 16:38:25 +0300

    Modified stream

--- yeabeseraD/Homeworks/Homework 6/Streams.hs
+++ yeabeseraD/Homeworks/Homework 6/Streams.hs
@@ -1,7 +1,7 @@
 data Stream a = Stream {value :: a, cons :: Stream a}
 
 instance Show a => Show (Stream a) where
-    show stream = take 20 (show stream)
+    show stream = take 20 (show $ streamToList stream) 
 
 streamToList :: Stream a -> [a]
 streamToList (Stream value tailList) = value:streamToList tailList
