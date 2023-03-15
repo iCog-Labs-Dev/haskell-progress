dbf4be649b4168a8d40218a5ffbeaa79de28a590
Yagth <yabuman46@gmail.com>
2023-03-15 16:28:37 +0300

    Solved RunLength

---
+++ yeabeseraD/Excersism/RunLenght.hs
@@ -0,0 +1,19 @@
+module RunLength (decode, encode) where
+import Data.List (group) 
+
+decode :: String -> String
+decode [] = []
+decode text@(x:xs)
+    | null countStr = x: decode xs
+    | otherwise = let [(count, str@(y:ys))] = countStr
+                  in replicate count y ++ decode ys
+    where countStr = reads (convertSpaces text) :: [(Int, String)]
+
+convertSpaces :: String -> String
+convertSpaces =  map (\x -> if x == ' ' then '_' else x)
+
+encode :: String -> String
+encode text = concatMap countAndAttach (group text)
+    where countAndAttach x = if length x > 1 
+                                then show (length x) ++ [head x] 
+                                else [head x]
