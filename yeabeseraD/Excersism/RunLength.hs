6ff9950accc21063b50acbee31256c2da8d6324d
Yagth <yabuman46@gmail.com>
2023-03-15 16:32:18 +0300

    Minor bug fix

--- yeabeseraD/Excersism/RunLength.hs
+++ yeabeseraD/Excersism/RunLength.hs
@@ -5,13 +5,15 @@ decode :: String -> String
 decode [] = []
 decode text@(x:xs)
     | null countStr = x: decode xs
-    | otherwise = let [(count, str@(y:ys))] = countStr
+    | otherwise = let [(count, str)] = countStr; (y:ys) = convertToSpaces str
                   in replicate count y ++ decode ys
     where countStr = reads (convertSpaces text) :: [(Int, String)]
 
 convertSpaces :: String -> String
 convertSpaces =  map (\x -> if x == ' ' then '_' else x)
 
+convertToSpaces :: String -> String
+convertToSpaces = map (\x -> if x == '_' then ' ' else x)
 encode :: String -> String
 encode text = concatMap countAndAttach (group text)
     where countAndAttach x = if length x > 1 
