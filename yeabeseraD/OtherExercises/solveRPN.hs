4a6ec16723f86d89495f071e9ad1b5766871511d
Yagth <yabuman46@gmail.com>
2023-03-16 14:26:24 +0300

    Modified the RPN to hanlde errors

--- yeabeseraD/OtherExercises/solveRPN.hs
+++ yeabeseraD/OtherExercises/solveRPN.hs
@@ -1,11 +1,20 @@
 import Data.List
+import Control.Monad (liftM, foldM)
+solveRpn :: String -> Maybe Double
+solveRpn xs = do
+  [result] <- foldM calculate [] (words xs)
+  return result
+  
+calculate :: [Double] -> String -> Maybe [Double]
+calculate (x:y:ys) "*" = return((x * y):ys)
+calculate (x:y:ys) "+" = return((x + y):ys)
+calculate (x:y:ys) "-" = return ((y - x):ys)
+calculate (x:y:ys) "/" = return ((y / x):ys)
+calculate (x:y:ys) "^" = return ((y ** x):ys)
+calculate (x:ys) "log" = return (log x: ys)
+calculate xs numberString = liftM (:xs) (readMaybe numberString)
 
-solveRpn :: String -> Float
-solveRpn = head . foldl calculate [] . words
-  where calculate (x:y:ys) "*" = (x * y):ys
-        calculate (x:y:ys) "+" = (x + y):ys
-        calculate (x:y:ys) "-" = (y - x):ys
-        calculate (x:y:ys) "/" = (y / x):ys
-        calculate (x:y:ys) "^" = (y ** x):ys
-        calculate (x:ys) "log" = log x: ys
-        calculate xs numberString = read numberString:xs
+readMaybe :: String -> Maybe Double
+readMaybe numberString = case reads numberString of
+  [(x, "")] -> return x
+  _ -> Nothing
