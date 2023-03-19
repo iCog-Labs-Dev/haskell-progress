1b06b9c88202790dbddcb2409da29fea8ef0f526
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-03-19 21:46:38 +0300

    modefing project

--- YeabNigusse/Projects/JSON Parsor In Haskell/Main.hs
+++ YeabNigusse/Projects/JSON Parsor In Haskell/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+data JsonValue = JsonNull 
+                | JsonBool Bool 
+                | JsonNumber Integer 
+                | JasonString String 
+                | JsonArray [JsonValue] 
+                | JsonObject [(String, JsonValue)] deriving (Show, Eq)-- this are possible values that 
+                                                                      -- a string is being parsed
+            
+newtype Parsor a = Parsor {runParser :: String -> Maybe (String, a)}{-- a string my be parsed to a some json value with other string that can be chained 
+                                                                         or (no proper error reporting)   --}
+jsonValue :: Parsor JsonValue
+jsonValue = undefined
+
+
 main :: IO ()
 main =  undefined
+
