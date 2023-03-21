32a7e98da2ba96d2a9bee3253e9d0722e53a6eb3
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-03-21 17:38:58 +0300

    adding pascal's triangle

--- YeabNigusse/RandomExcercise/matrix.hs
+++ YeabNigusse/RandomExcercise/matrix.hs
@@ -1,4 +1,3 @@
--- create user input matrix 
 
 main = do
     putStrLn "Enter row number"
@@ -12,10 +11,10 @@ getmatrix (a, b) = unlines $ map row [1 .. a]
                        where row n = unwords $ map column [1 .. b]
                               where column m = show (n * m + m - n)
 
-getlist :: Int -> [[Integer]]
-getlist a = map row [1 .. a]
-                where row n = map column [0 .. n-1]
-                       where column m = factorial n / (factorial (n-m) * factorial m)
+getlist :: Int -> [[Int]]
+getlist a = map row [0 .. a-1]
+                where row n = map column [0 .. n]
+                       where column m = factorial n `div` (factorial m * factorial (n-m))
 
 factorial :: (Eq t, Num t) => t -> t
 factorial 0 = 1
