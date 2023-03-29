0a6e7d8037a0dbfeb917353bbdc55ea7351a7703
Yagth <yabuman46@gmail.com>
2023-03-29 14:37:57 +0300

    Made the server statefull

--- yeabeseraD/iohkedu/networkServer/app/Main.hs
+++ yeabeseraD/iohkedu/networkServer/app/Main.hs
@@ -2,24 +2,29 @@ module Main where
 
 import Network
 import Control.Concurrent
-import Data.Char
+import Control.Concurrent.STM
 import Control.Monad
 import System.IO
-import System.IO.Error
 
 main :: IO ()
 main = do
     s <- listenOn $ PortNumber 8657
+    conns <- newTVarIO 0
+    putStrLn "Listening on port number 8657..."
     forever $ do
         (h, n, p) <- accept s
-        putStrLn $ "accepted hostname " ++ show n ++ " on portnumber "++ show p
-        forkIO (handleClient h)
+        putStrLn $ "accepted hostname: " ++ n ++ " on portnumber: "++ show p
+        forkFinally (handleClient h conns) $
+            const $ do
+                atomically $ modifyTVar conns pred
+                hClose h
 
-handleClient :: Handle -> IO ()
-handleClient h = do
+handleClient :: Handle -> TVar Int -> IO ()
+handleClient h conns = do
     hSetBuffering h LineBuffering
-    catchIOError (forever $ do
-            l <- hGetLine h
-            hPutStrLn h (map toUpper l))
-            (\e -> putStrLn $ "Something went wrong: \n\t " ++ show e ++ "\n I'm out of here.")
+    atomically $ modifyTVar conns succ
+    forever $ do
+            _ <- hGetLine h
+            clients <- readTVarIO conns
+            hPrint h clients
 
