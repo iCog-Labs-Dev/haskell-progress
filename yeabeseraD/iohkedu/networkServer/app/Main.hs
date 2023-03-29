bb2f9a077eb175b3c54e545ab1266d5c2e115f5a
Yagth <yabuman46@gmail.com>
2023-03-29 14:51:33 +0300

    Made the server to notify changes to clients

--- yeabeseraD/iohkedu/networkServer/app/Main.hs
+++ yeabeseraD/iohkedu/networkServer/app/Main.hs
@@ -9,22 +9,37 @@ import System.IO
 main :: IO ()
 main = do
     s <- listenOn $ PortNumber 8657
-    conns <- newTVarIO 0
+    conns <- newTVarIO []
+    void $ forkIO $ monitor 0 conns
     putStrLn "Listening on port number 8657..."
     forever $ do
         (h, n, p) <- accept s
         putStrLn $ "accepted hostname: " ++ n ++ " on portnumber: "++ show p
         forkFinally (handleClient h conns) $
             const $ do
-                atomically $ modifyTVar conns pred
+                atomically $ modifyTVar conns (filter (/=h))
                 hClose h
 
-handleClient :: Handle -> TVar Int -> IO ()
+handleClient :: Handle -> TVar [Handle] -> IO ()
 handleClient h conns = do
     hSetBuffering h LineBuffering
-    atomically $ modifyTVar conns succ
+    atomically $ modifyTVar conns (h:)
     forever $ do
             _ <- hGetLine h
-            clients <- readTVarIO conns
+            handles <- readTVarIO conns
+            let clients = length handles
             hPrint h clients
+
+monitor :: Int -> TVar [Handle] -> IO ()
+monitor oldCount conns = do
+    newCount <- atomically $ do
+        hs <- readTVar conns
+        let c = length hs
+        if c == oldCount
+            then retry
+            else return c
+    
+    hs <- readTVarIO conns
+    mapM_ (`hPrint` newCount) hs
+    monitor newCount conns
 
