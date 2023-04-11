aaf3225d7a17fdd767d53ce3f51ffb314e803ac8
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-04-11 14:16:19 +0300

    modefing files

--- YeabNigusse/Notes/Real World Haskell/chapter4.hs
+++ YeabNigusse/Notes/Real World Haskell/chapter4.hs
@@ -1,17 +1,42 @@
+import System.Environment (getArgs)
+
+
+interactWith function inputFile outputFile = do
+    input <- readFile inputFile
+    writeFile outputFile (function input)
+
+main = mainWith myFunction
+    where mainWith function = do
+        args <- getArgs
+              case args of
+                  [input,output] -> interactWith function input output
+                  _ -> putStrLn "error: exactly two arguments needed"
+
+-- replace "id" with the name of our function below
+myFunction = id
+
+
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead (x:xs) = Just x
 
-safeLast :: [a] -> Maybe [a]
-safeLast [] = Nothing
-safeLast lst
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail lst
           | length lst == 1 = Just []
           | otherwise = Just (tail lst)
 
-safeTail :: [a] -> Maybe a
-safeTail[] = Nothing
-safeTail lst@(x:xs)
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast lst = Just (last lst)
+
+safeInit :: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit lst 
           | length lst == 1 = Just []
-          | otherwise = Just xs
+          | otherwise = Just (init lst)
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith p lst = filter p lst : filter (not . p) lst : []
 
 
