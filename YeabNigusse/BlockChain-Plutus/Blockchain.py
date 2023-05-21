b2672bcea1698bdab49e263b52951ac1116b10bb
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-05-21 23:38:08 +0300

    building a block chain

--- YeabNigusse/BlockChain-Plutus/Blockchain.py
+++ YeabNigusse/BlockChain-Plutus/Blockchain.py
@@ -1,10 +1,42 @@
-class Blockchain(object):
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
+from flask import Flask
+
+
+class Blockchain(object): #what does this class do?
     def __init__(self):
-        self.chain = []
+        self.chain = []#containes list of chained blocks
+        self.current_transactions = []# contain list of current transaction
+    
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
         self.current_transactions = []
-    def new_blocks(self):
-        pass
-    def new_transaction(self, sender, recipient, amount):
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):#append a transaction to current transaction
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -12,12 +44,50 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+    
     def hash(block):
-        pass
-    def last_block(self):
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
 
-block = {
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def last_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+block = {# this is a block information
     'index': 1,
     'timestamp': 1506057125.900785,
     'transactions': [
